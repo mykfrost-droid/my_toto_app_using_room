f31accb160179d9d820e85f554bdd312d97aead0
MichaelOtieno <okellofrost@gmail.com>
2026-08-11 14:13:44 +0300

    adding room library and adding project structure folders

--- app/src/main/java/co/ke/eurochef/android/mytodos/main/MainActivity.java
+++ app/src/main/java/co/ke/eurochef/android/mytodos/main/MainActivity.java
@@ -1,4 +1,4 @@
-package co.ke.eurochef.android.mytodos;
+package co.ke.eurochef.android.mytodos.main;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import co.ke.eurochef.android.mytodos.R;
 
 public class MainActivity extends AppCompatActivity {
 
